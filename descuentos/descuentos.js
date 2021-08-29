51b8be633ded2a8533ee89ce67234a86dc18121d
Aeros RH <cesar.reyes.89062@gmail.com>
2021-08-28 22:12:40 -0500

    Agregando seccion de cupones

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,8 +1,23 @@
 // const precioOriginal = 120;
 // const descuento = 35;
 
-function calcularPrecioConDescuento(precio, descuento){
-    const porcentajePrecioDescuento = 100 - descuento;
+
+function calcularPrecioConDescuento(precio, descuento, coupon){
+
+    let couponDiscount;
+    switch(coupon) {
+        case "APPLE":
+        couponDiscount = 10;
+        break;
+        case "MICROSOFT":
+        couponDiscount = 15;
+        break;
+        case "GOOGLE":
+        couponDiscount = 20;
+        break;
+    }
+
+    const porcentajePrecioDescuento = 100 - descuento - couponDiscount;
     const precioConDescuento = (precio * porcentajePrecioDescuento) / 100;
 
     return precioConDescuento;
@@ -13,11 +28,13 @@ function calculatePriceDiscount(){
     const priceValue = price.value;
     const discount = document.getElementById("inputDiscount");
     const discountValue = discount.value;
+    const couponText = document.getElementById("inputCoupon");
+    const couponValue = couponText.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue, couponValue);
 
     const resulPrice = document.getElementById("resultPrice")
-    resulPrice.innerText = "EL precio con descuento es: $" + precioConDescuento;
+    resulPrice.innerText = "EL precio final es: $" + precioConDescuento;
 }
 
 // console.log({
